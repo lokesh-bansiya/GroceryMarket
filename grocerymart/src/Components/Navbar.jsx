b5eae2a09fb9d3f016609c5ae7561da3ef8af3de
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-01-24 10:37:09 +0530

    Grocery Mart

--- grocerymart/src/Components/Navbar.jsx
+++ grocerymart/src/Components/Navbar.jsx
@@ -1,292 +1,141 @@
 import {
-    Box,
-    Flex,
-    Text,
-    IconButton,
-    Button,
-    Stack,
-    Collapse,
-    Icon,
-    Input,
-    Link,
-    Popover,
-    PopoverTrigger,
-    PopoverContent,
-    useColorModeValue,
-    useBreakpointValue,
-    useDisclosure,
-  } from '@chakra-ui/react';
-  import {
-    HamburgerIcon,
-    CloseIcon,
-    ChevronDownIcon,
-    ChevronRightIcon,
-  } from '@chakra-ui/icons';
-  import logo from "../Assets/redCart.png";
-  
-  export default function Navbar() {
-    const { isOpen, onToggle } = useDisclosure();
-  
-    return (
-      <Box>
-        <Flex
-          bg={useColorModeValue('white', 'gray.800')}
-          color={useColorModeValue('gray.600', 'white')}
-          minH={'60px'}
-          py={{ base: 2 }}
-          px={{ base: 4 }}
-          borderBottom={1}
-          borderStyle={'solid'}
-          borderColor={useColorModeValue('gray.200', 'gray.900')}
-          align={'center'}>
+  Box,
+  Flex,
+  Button,
+  Input,
+  Image,
+  useDisclosure
+} from '@chakra-ui/react';
+import logo from "../Assets/redCart.png";
+import cart from "../Assets/cart.jpg";
+import user from "../Assets/user.png";
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
+import { useState } from 'react';
+import SignUp from '../Modal/SignUp';
+import Login from '../Modal/Login';
+
+export default function Navbar() {
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [state, setState] = useState(true);
+
+  return (
+    <Box 
+      width="100%" 
+      margin="auto">
+      <Flex
+        paddingRight="2%" 
+        paddingLeft="2%" 
+        width="100%"
+        flexDirection="row"
+        justifyContent="space-between"
+        boxShadow="rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgb(209, 213, 219) 0px 0px 0px 1px inset"
+        alignItems="center"
+        height="auto"
+        
+      >
+        <Box
+          width={{base: "30%", sm:"25%", md:"20%", lg:"15%"}}
+        >
+          <Image width="100%" src={logo} alt='logo' />
+        </Box>
+        <Box 
+          display={{ base: "none", sm: "none", md: "flex", lg: "flex" }} 
+          width={{base: "40%", sm:"50%", md:"60%", lg:"60%"}}>
           <Flex
-            flex={{ base: 1, md: 'auto' }}
-            ml={{ base: -2 }}
-            display={{ base: 'flex', md: 'none' }}>
-            <IconButton
-              onClick={onToggle}
-              icon={
-                isOpen ? <CloseIcon w={3} h={3} /> : <HamburgerIcon w={5} h={5} />
-              }
-              variant={'ghost'}
-              aria-label={'Toggle Navigation'}
-            />
+            width="100%"
+            alignItems="center"
+          >
+            <Box marginRight="3%" width={{ base: "0%", sm: "40%", md: "40%", lg: "40%" }}>
+              <Flex
+                justifyContent="space-around"
+                alignItems="center"
+                flexDirection="row"
+                width={{ base: "0%", sm: "100%", md: "100%", lg: "100%" }}
+              >
+                <Flex
+                  justifyContent="space-around"
+                  alignItems="center"
+                  width={{ base: "0%", sm: "100%", md: "100%", lg: "100%" }}
+                  display={{ base: "none", sm: "flex", md: "flex", lg: "flex" }}
+                >
+                  <Box fontSize={{ base: "70%", sm: "70%", md: "100%", lg: "150%" }} fontWeight="bold">
+                    Products
+                  </Box>
+                  <Box fontSize={{ base: "70%", sm: "70%", md: "100%", lg: "150%" }} fontWeight="bold">
+                    Category
+                  </Box>
+                </Flex>
+              </Flex>
+            </Box>
+            <Box
+              display={{base: "none", sm:"block", md:"block",lg: "block"}}
+              width={{ base: "90%", sm: "50%", md: "50%", lg: "50%" }}
+            >
+              <Input marginTop="5%" marginBottom="5%" variant='outline' placeholder='Search' size={{base: "sm", sm: "md", md: "md", lg:"lg"}} />
+            </Box>
           </Flex>
-          <Flex flex={{ base: 1 }} justify={{ base: 'center', md: 'start' }}>
-            <Text
-              width="15%"
-              textAlign={useBreakpointValue({ base: 'center', md: 'left' })}
-              fontFamily={'heading'}
-              color={useColorModeValue('gray.800', 'white')}>
-              <img style={{width: "100%"}} src={logo} alt="logo" />
-            </Text>
-  
-            <Flex alignItems="center" width={"100%"}  display={{ base: 'none', md: 'flex' }} ml={10}>
-              <DesktopNav />
-              
-            </Flex>
-          </Flex>
-  
-          <Stack
-            flex={{ base: 1, md: 0 }}
-            justify={'flex-end'}
-            direction={'row'}
-            spacing={6}>
-            <Button
-              as={'a'}
-              fontSize={'sm'}
-              fontWeight={500}
-              color="gray.700"
-              variant={'link'}
-              href={'#'}>
-              Sign In
-            </Button>
-            <Button
-              display={{ base: 'none', md: 'inline-flex' }}
-              fontSize={'sm'}
-              fontWeight={500}
-              color={'white'}
-              bg={'pink.400'}
-              href={'#'}
-              _hover={{
-                bg: 'pink.300',
-              }}>
-              Sign Up
-            </Button>
-          </Stack>
-        </Flex>
-  
-        <Collapse in={isOpen} animateOpacity>
-          <MobileNav />
-        </Collapse>
-      </Box>
-    );
-  }
-  
-    const DesktopNav = () => {
-    const linkColor = useColorModeValue('gray.800', 'gray.500');
-    const linkHoverColor = useColorModeValue('gray.800', 'white');
-    const popoverContentBgColor = useColorModeValue('white', 'gray.800');
-  
-    return (
-      <Stack direction={'row'} spacing={4}>
-        {NAV_ITEMS.map((navItem) => (
-          <Box width="fit-content" key={navItem.label}>
-            <Popover trigger={'hover'} placement={'bottom-start'}>
-              <PopoverTrigger>
-                <Link
-                  p={2}
-                  href={navItem.href ?? '#'}
-                  fontSize={'sm'}
-                  fontWeight={500}
-                  color={linkColor}
-                  _hover={{
-                    textDecoration: 'none',
-                    color: linkHoverColor,
-                  }}>
-                  {navItem.label}
-                </Link>
-              </PopoverTrigger>
-  
-              {navItem.children && (
-                <PopoverContent
-                  border={0}
-                  boxShadow={'xl'}
-                  bg={popoverContentBgColor}
-                  p={4}
-                  rounded={'xl'}
-                  minW={'sm'}>
-                  <Stack>
-                    {navItem.children.map((child) => (
-                      <DesktopSubNav key={child.label} {...child} />
-                    ))}
-                  </Stack>
-                </PopoverContent>
-              )}
-            </Popover>
-          </Box>
-        ))}
-      </Stack>
-    );
-  };
-  
-  const DesktopSubNav = ({ label, href, subLabel }) => {
-    return (
-      <Link
-        href={href}
-        role={'group'}
-        display={'block'}
-        p={2}
-        rounded={'md'}
-        _hover={{ bg: useColorModeValue('pink.50', 'gray.900') }}>
-        <Stack direction={'row'} align={'center'}>
-          <Box>
-            <Text
-              transition={'all .3s ease'}
-              _groupHover={{ color: 'pink.400' }}
-              fontWeight={500}>
-              {label}
-            </Text>
-            <Text fontSize={'sm'}>{subLabel}</Text>
-          </Box>
+        </Box>
+        <Box width={{base: "40%", sm:"30%", md:"20%", lg:"20%"}}>
           <Flex
-            transition={'all .3s ease'}
-            transform={'translateX(-10px)'}
-            opacity={0}
-            _groupHover={{ opacity: '100%', transform: 'translateX(0)' }}
-            justify={'flex-end'}
-            align={'center'}
-            flex={1}>
-            <Icon color={'pink.400'} w={5} h={5} as={ChevronRightIcon} />
+            flexDirection="row"
+            alignItems="center"
+            justifyContent="space-around"
+          >
+            <Button 
+              fontSize={{base: "60%", sm:"70%", md:"100%", lg:"100%"}} 
+              size={{ base: "sm", sm: "sm", md: "md", lg: "lg" }} 
+              onClick={onOpen}
+              bg={"blue.300"}>
+                Login
+            </Button>
+            <Login isOpen={isOpen} onClose={onClose}/>
+            
+            <Button 
+              fontSize={{base: "60%", sm:"70%", md:"100%", lg:"100%"}} 
+              size={{ base: "sm", sm: "sm", md: "md", lg: "lg",xl: "lg" }} 
+              bg={"pink.500"} 
+              onClick={onOpen}
+              color={"white"}>
+                Sign Up
+            </Button>
+            <SignUp isOpen={isOpen} onClose={onClose}/>
           </Flex>
-        </Stack>
-      </Link>
-    );
-  };
-  
-  const MobileNav = () => {
-    return (
-      <Stack
-        bg={useColorModeValue('white', 'gray.800')}
-        p={4}
-        display={{ md: 'none' }}>
-        {NAV_ITEMS.map((navItem) => (
-          <MobileNavItem key={navItem.label} {...navItem} />
-        ))}
-      </Stack>
-    );
-  };
-  
-  const MobileNavItem = ({ label, children, href }) => {
-    const { isOpen, onToggle } = useDisclosure();
-  
-    return (
-      <Stack spacing={4} onClick={children && onToggle}>
-        <Flex
-          py={2}
-          as={Link}
-          href={href ?? '#'}
-          justify={'space-between'}
-          align={'center'}
-          _hover={{
-            textDecoration: 'none',
-          }}>
-          <Text
-            fontWeight={600}
-            color={useColorModeValue('gray.600', 'gray.200')}>
-            {label}
-          </Text>
-          {children && (
-            <Icon
-              as={ChevronDownIcon}
-              transition={'all .25s ease-in-out'}
-              transform={isOpen ? 'rotate(180deg)' : ''}
-              w={6}
-              h={6}
-            />
-          )}
-        </Flex>
-  
-        <Collapse in={isOpen} animateOpacity style={{ marginTop: '0!important' }}>
-          <Stack
-            mt={2}
-            pl={4}
-            borderLeft={1}
-            borderStyle={'solid'}
-            borderColor={useColorModeValue('gray.200', 'gray.700')}
-            align={'start'}>
-            {children &&
-              children.map((child) => (
-                <Link key={child.label} py={2} href={child.href}>
-                  {child.label}
-                </Link>
-              ))}
-          </Stack>
-        </Collapse>
-      </Stack>
-    );
-  };
-  
-  const NAV_ITEMS = [
-    {
-      label: 'Brand',
-      children: [
-        {
-          label: 'Explore Design Work',
-          subLabel: 'Trending Design to inspire you',
-          href: '#',
-        },
-        {
-          label: 'New & Noteworthy',
-          subLabel: 'Up-and-coming Designers',
-          href: '#',
-        },
-      ],
-    },
-    {
-      label: 'Non-veg',
-      children: [
-        {
-          label: 'Products',
-          subLabel: 'Find your dream design job',
-          href: '#',
-        },
-        {
-          label: 'Freelance Projects',
-          subLabel: 'An exclusive list for contract work',
-          href: '#',
-        },
-      ],
-    },
-    {
-      label: 'Beauty',
-      href: '#',
-    },
-    {
-      label: 'Vegetables',
-      href: '#',
-    },
-    {
-        label: <Input width="160%" variant='outline' placeholder='search' />
-    }
-  ];
+        </Box>
+      </Flex>
+      <Flex
+        flexDirection="row"
+        paddingRight="2%" 
+        paddingLeft="2%" 
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Box 
+          display="block"
+          width="10%"
+        >
+          <Box display={{base: "block", sm:"none", md:"none",lg: "none", xl: "none"}}>
+            {state? <HamburgerIcon fontSize="160%"/> : <CloseIcon fontSize="150%"/>}
+          </Box>
+        </Box>
+        <Box width={{ base: "30%", sm: "20%", md: "12%", lg: "10%" }}>
+          <Flex
+            flexDirection="row"
+            alignItems="center"
+            width="100%"
+            border="1px solid blue"
+            bg={"blue.400"}
+            borderRadius="10px"
+            justifyContent="space-around">
+            <Box width="30%">
+              <Image margin="3%" borderRadius="50%" width="100%" src={cart} alt="Cart" />
+            </Box>
+            <Box width="30%">
+              <Image margin="3%" borderRadius="50%" width="100%" src={user} />
+            </Box>
+          </Flex>
+        </Box>
+      </Flex>
+    </Box>
+  )
+}
