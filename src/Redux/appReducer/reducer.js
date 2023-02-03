7d76f366fd38899d2a556b9ad218d5e4fddb4026
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-02-03 11:35:39 +0530

    Grocery Mart

--- src/Redux/appReducer/reducer.js
+++ src/Redux/appReducer/reducer.js
@@ -42,77 +42,65 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         isLoadingBeauty: true,
-        isError: false,
       };
     case types.GET_BY_CATEGORY_BEAUTY_SUCCESS:
       return {
         ...state,
         itemsByCategoryBeauty: payload,
         isLoadingBeauty: false,
-        isError: false,
       };
     case types.GET_BY_CATEGORY_BEAUTY_FAILURE:
       return {
         ...state,
         isLoadingBeauty: false,
-        isError: true,
       };
     case types.GET_BY_CATEGORY_VEGETABLES_REQUEST:
       return {
         ...state,
         isLoadingVegetables: true,
-        isError: false,
       };
     case types.GET_BY_CATEGORY_VEGETABLES_SUCCESS:
       return {
         ...state,
         itemsByCategoryVegetable: payload,
         isLoadingVegetables: false,
-        isError: false,
       };
     case types.GET_BY_CATEGORY_VEGETABLES_FAILURE:
       return {
         ...state,
         isLoadingVegetables: false,
-        isError: true,
       };
     case types.GET_BY_CATEGORY_BAKERY_REQUEST:
       return {
         ...state,
         isLoadingBakery: true,
-        isError: false,
       };
     case types.GET_BY_CATEGORY_BAKERY_SUCCESS:
       return {
         ...state,
         itemsByCategoryBakery: payload,
         isLoadingBakery: false,
-        isError: false,
       };
     case types.GET_BY_CATEGORY_BAKERY_FAILURE:
       return {
         ...state,
         isLoadingBakery: false,
-        isError: true,
       };
     case types.GET_ALL_PRODUCTS_REQUEST:
       return {
         ...state,
         isLoadingAllProduct: true,
-        isError: false,
       };
     case types.GET_ALL_PRODUCTS_SUCCESS:
       return {
         ...state,
         allProducts: payload,
         isLoadingAllProduct: false,
-        isError: false,
       };
     case types.GET_ALL_PRODUCTS_FAILURE:
       return {
         ...state,
         isLoadingAllProduct: false,
-        isError: true,
       };
     case types.GET_SINGLE_PRODUCT_REQUEST:
       return {
